d2eab38e838d5adb4145fc90ee262ac0c89a54e0
senchromatic <samuelfzhang@gmail.com>
2017-05-29 21:39:13 -0400

    random 32->64 bit integer, sanity check

--- src/math/gcd.cpp
+++ src/math/gcd.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
+#include <cassert>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
 
+#include "../../lib/random.h"
 #include "../../lib/timer.h"
 #include "../../lib/type.h"
 
@@ -10,9 +12,14 @@
 using Benchmarking::ll;
 using Benchmarking::Timer;
 
+using Benchmarking::rand_64;
+using std::__gcd;
+using std::max;
+using std::min;
 using std::rand;
 
 constexpr ll NUM_ELEMENTS = 1e6;
+constexpr bool PARANOIA = false;
 
 ll base = 0;
 std::ofstream fout("logs/gcd.txt");
@@ -21,8 +28,8 @@ std::ofstream fout("logs/gcd.txt");
 void test_stl_gcd() {
     Timer timer(NUM_ELEMENTS, nullptr);
     for (ll i = 0; i < NUM_ELEMENTS; i++) {
-        ll n1 = rand(), n2 = rand();
-        std::__gcd(n1, n2);
+        ll n1 = rand_64(), n2 = rand_64();
+        __gcd(n1, n2);
     }
     fout << "stl::gcd(n1, n2): " << timer.report(base) << '\n';
 }
@@ -36,20 +43,26 @@ inline ll euclidean_algorithm(const ll &a, const ll &b) {
 void test_custom_gcd() {
     Timer timer(NUM_ELEMENTS, nullptr);
     for (ll i = 0; i < NUM_ELEMENTS; i++) {
-        ll n1 = rand(), n2 = rand();
-        euclidean_algorithm(std::max(n1, n2), std::min(n1, n2));
+        ll n1 = rand_64(), n2 = rand_64();
+        euclidean_algorithm(max(n1, n2), min(n1, n2));
     }
     fout << "custom::gcd(n1, n2): " << timer.report(base) << '\n';
 }
 
 int main() {
-    std::srand(std::time(0));
+    Benchmarking::random_seed();
     
     Timer loop(NUM_ELEMENTS, nullptr);
     for (ll i = 0; i < NUM_ELEMENTS; i++) {
-        ll n1 = rand(), n2 = rand();
+        ll n1 = rand_64(), n2 = rand_64();
     }
     base = loop.elapsed();
+
+	if (PARANOIA)
+		for (ll i = 0; i < NUM_ELEMENTS; i++) {
+			ll n1 = rand_64(), n2 = rand_64();
+			assert(__gcd(n1, n2) == euclidean_algorithm(max(n1, n2), min(n1, n2)));
+		}
 
     test_stl_gcd();
     test_custom_gcd();
